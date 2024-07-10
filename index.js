2d491a919a131117a5365996b220d5db0b7d8f43
barangievans <barangievans@gmail.com>
2024-07-10 21:10:50 +0300

    javascript functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// Function to handle Saturday activities
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function to handle Monday activities
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Function to wrap an adjective in a highlight
+function wrapAdjective(flair = '*') {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
+
+module.exports = saturdayFun;
+
